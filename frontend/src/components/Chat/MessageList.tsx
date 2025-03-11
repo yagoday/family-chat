2c9ae69970d660beef418d37a068b6948cef3340
Yaron Yagoda <yaronyagoda@gmail.com>
2025-03-11 14:32:23 +0200

    Refactor

--- frontend/src/components/Chat/MessageList.tsx
+++ frontend/src/components/Chat/MessageList.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import MessageItem from './MessageItem';
 
 interface Message {
